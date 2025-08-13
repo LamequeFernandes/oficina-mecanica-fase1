0591468b101cd5c8cc04e3190bf39ec82df421bd
Lameque Fernandes Azevedo <lamequesao@gmail.com>
2025-08-12 23:02:24 -0300

    adicao de arquivos de configuracao inicial

---
+++ app/core/config.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file='.env', env_file_encoding='utf-8'
+    )
+
+    USER_DB: str
+    PASSWORD_DB: str
+    HOST_DB: str
+    PORT_DB: str
+    DATABASE: str
+    SECRET_KEY: str
+    ALGORITHM: str
+    EMAIL_TEST_USER: str | None = None
+    PASSWORD_TEST_USER: str | None = None
+
+
+settings = Settings()  # type: ignore
