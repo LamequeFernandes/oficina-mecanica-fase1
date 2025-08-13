12d5c8fd60e2af3152ae25560deea453311c83c2
Lameque Fernandes Azevedo <lamequesao@gmail.com>
2025-08-13 19:24:40 -0300

    melhoria em swagger e divisao de representation de usuario em clientes e funcionarios

--- app/main.py
+++ app/main.py
@@ -1,32 +1,23 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from sqlalchemy.orm import Session
-from app.core.database import get_db, Base, engine
-# from app.core.security import criar_hash_senha
 
 from app.core.exceptions import tratar_erro_dominio
 from app.modules.usuario.presentation.routes import router as router_usuario
+from app.modules.usuario.presentation.routes_clientes import router as router_clientes
+from app.modules.usuario.presentation.routes_funcionarios import router as router_funcionarios
 from app.modules.veiculo.presentation.routes import router as router_veiculos
 from app.modules.ordem_servico.presentation.routes import router as router_ordem_servico
 
-app = FastAPI()
 
-# Cria as tabelas (apenas para desenvolvimento)
-from app.modules.usuario.infrastructure.models import *
-from app.modules.veiculo.infrastructure.models import *
-from app.modules.ordem_servico.infrastructure.models import *
-from app.modules.orcamento.infrastructure.models import *
+app = FastAPI(title='Oficina Mecânica - Fase 1', version='1.0.0')
 
-Base.metadata.create_all(engine)
 
 app.include_router(router_usuario, prefix='/usuarios', tags=['Usuários'])
+app.include_router(router_clientes, prefix='/usuarios/clientes', tags=['Clientes'])
+app.include_router(router_funcionarios, prefix='/usuarios/funcionarios', tags=['Funcionários'])
 app.include_router(router_veiculos, prefix='/veiculos', tags=['Veículos'])
 app.include_router(router_ordem_servico, prefix='/veiculo', tags=['Ordem de Serviço'])
 
-
-# @app.exception_handler(Exception)
-# async def handle_exceptions(request, exc):
-#     return tratar_erro_dominio(exc)
 
 @app.exception_handler(Exception)
 async def handle_exceptions(request, exc):
--- app/modules/usuario/presentation/routes.py
+++ app/modules/usuario/presentation/routes.py
@@ -2,9 +2,8 @@ from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from app.core.database import get_db
-from app.core.dependencies import obter_cliente_logado, obter_id_usuario_logado, obter_usuario_logado
-from app.modules.usuario.application.use_cases import AlterarClienteUseCase, AlterarFuncionarioUseCase, ConsultarClienteUseCase, ConsultarFuncionarioUseCase, CriarClienteUseCase, CriarFuncionarioUseCase, LoginUseCase, RemoverClienteUseCase, RemoverFuncionarioUseCase
-from app.modules.usuario.application.dto import ClienteInputDTO, ClienteOutputDTO, FuncionarioInputDTO, FuncionarioOutputDTO, LoginOutputDTO
+from app.modules.usuario.application.use_cases import LoginUseCase
+from app.modules.usuario.application.dto import LoginOutputDTO
 from app.modules.usuario.infrastructure.repositories import AuthRepository
 
 
@@ -21,105 +20,3 @@ def login(
         return use_case.execute(form_data.username, form_data.password) # type: ignore
     except ValueError as e:
         raise HTTPException(status_code=401, detail=str(e))
-    
-
-@router.post("/clientes/cadastrar", response_model=ClienteOutputDTO, status_code=201)
-def criar_cliente(
-    cliente_data: ClienteInputDTO,
-    db: Session = Depends(get_db)
-):
-    use_case = CriarClienteUseCase(db)
-    return use_case.executar(cliente_data)
-
-
-@router.get("/clientes/{cliente_id}", response_model=ClienteOutputDTO)
-def consultar_cliente(
-    cliente_id: int,
-    usuario_id = Depends(obter_id_usuario_logado), 
-    db: Session = Depends(get_db)
-):
-    use_case = ConsultarClienteUseCase(db)
-    return use_case.executar_consulta_por_id(cliente_id)
-
-
-# TODO: AJUSTAR ENDPOINT
-@router.get("/clientes/cpfcnpj/{cpf_cnpj}", response_model=ClienteOutputDTO)
-def consultar_cliente_por_cpf_cnpj(
-    cpf_cnpj: str,
-    usuario_id = Depends(obter_id_usuario_logado), 
-    db: Session = Depends(get_db)
-):
-    use_case = ConsultarClienteUseCase(db)
-    return use_case.executar_consulta_por_cpf_cnpj(cpf_cnpj)
-
-
-@router.put("/clientes/{cliente_id}", response_model=ClienteOutputDTO)
-def alterar_cliente(
-    cliente_id: int,
-    cliente_data: ClienteInputDTO,
-    cliente = Depends(obter_cliente_logado), 
-    db: Session = Depends(get_db)
-):
-    use_case = AlterarClienteUseCase(db, cliente)
-    return use_case.executar(cliente_id, cliente_data)
-
-
-@router.delete("/clientes/{cliente_id}", status_code=204)
-def remover_cliente(
-    cliente_id: int,
-    usuario = Depends(obter_usuario_logado), 
-    db: Session = Depends(get_db)
-):
-    use_case = RemoverClienteUseCase(db, usuario)
-    use_case.executar(cliente_id)
-
-
-@router.post("/funcionarios/cadastrar", response_model=FuncionarioOutputDTO, status_code=201)
-def criar_funcionario(
-    funci_data: FuncionarioInputDTO,
-    db: Session = Depends(get_db)
-):
-    use_case = CriarFuncionarioUseCase(db)
-    return use_case.executar(funci_data)
-
-
-@router.get("/funcionarios/{funcionario_id}", response_model=FuncionarioOutputDTO)
-def consultar_funcionario(
-    funcionario_id: int,
-    usuario_id = Depends(obter_id_usuario_logado), 
-    db: Session = Depends(get_db)
-):
-    use_case = ConsultarFuncionarioUseCase(db)
-    return use_case.executar_consulta_por_id(funcionario_id)
-
-
-# TODO: AJUSTAR ROTA
-@router.get("/funcionarios/matricula/{matricula}", response_model=FuncionarioOutputDTO)
-def consultar_funcionario_por_matricula(
-    matricula: int,
-    usuario_id = Depends(obter_id_usuario_logado), 
-    db: Session = Depends(get_db)
-):
-    use_case = ConsultarFuncionarioUseCase(db)
-    return use_case.executar_consulta_por_matricula(matricula)
-
-
-@router.put("/funcionarios/{funcionario_id}", response_model=FuncionarioOutputDTO)
-def alterar_funcionario(
-    funcionario_id: int,
-    funcionario_data: FuncionarioInputDTO,
-    usuario = Depends(obter_usuario_logado), 
-    db: Session = Depends(get_db)
-):
-    use_case = AlterarFuncionarioUseCase(db, usuario)
-    return use_case.executar(funcionario_id, funcionario_data)
-
-
-@router.delete("/funcionarios/{funcionario_id}", status_code=204)
-def remover_funcionario(
-    funcionario_id: int,
-    usuario = Depends(obter_usuario_logado), 
-    db: Session = Depends(get_db)
-):
-    use_case = RemoverFuncionarioUseCase(db, usuario)
-    use_case.executar(funcionario_id)
