0ce39902ed0f087211166e37cf258100b69f6bc8
Lameque Fernandes Azevedo <lamequesao@gmail.com>
2025-08-12 23:13:35 -0300

    adicao de dependencies.py, exceptions.py e security.py

---
+++ app/core/exceptions.py
@@ -0,0 +1,63 @@
+
+from fastapi import HTTPException, status
+
+
+class OrdemServicoNotFoundError(Exception):
+    """Ordem de serviço não encontrada."""
+    def __init__(self, ordem_servico_id: int | None = None):
+        super().__init__(f"Ordem de serviço {f'ID {ordem_servico_id}' if ordem_servico_id else ''}não encontrado.")
+        self.ordem_servico_id = ordem_servico_id
+
+
+class VeiculoNotFoundError(Exception):
+    """Veículo foi encontrado."""
+    def __init__(self, veiculo_id: int | None = None):
+        super().__init__(f"Veículo {f'ID {veiculo_id}' if veiculo_id else ''}não encontrado.")
+        self.veiculo_id = veiculo_id
+
+
+class ApenasAdminPodeAcessarError(Exception):
+    """Apenas administradores podem acessar."""
+    def __init__(self):
+        super().__init__("Apenas administradores podem acessar.")
+
+
+class ApenasMecanicosPodemAcessarError(Exception):
+    """Apenas mecanicos podem acessar"""
+    def __init__(self):
+        super().__init__("Apenas mecanicos podem acessar")
+
+
+class ApenasClientesPodemAcessarError(Exception):
+    """Apenas clientes podem acessar"""
+    def __init__(self):
+        super().__init__("Apenas clientes podem acessar")
+
+
+class TokenInvalidoError(Exception):
+    """Token inválido ou expirado"""
+    def __init__(self):
+        super().__init__("Token inválido ou expirado")
+
+
+class ValidacaoTokenError(Exception):
+    """Erro na validação do token"""
+    def __init__(self):
+        super().__init__("Erro na validação do token")
+
+
+def tratar_erro_dominio(error: Exception) -> HTTPException:
+    match error:
+        case OrdemServicoNotFoundError() | VeiculoNotFoundError():
+            return HTTPException(status.HTTP_404_NOT_FOUND, detail=str(error))
+        case ApenasAdminPodeAcessarError() \
+            | ApenasMecanicosPodemAcessarError() \
+            | ApenasClientesPodemAcessarError():
+            return HTTPException(status.HTTP_403_FORBIDDEN, detail=str(error))
+        case TokenInvalidoError() \
+            | ValidacaoTokenError():
+            return HTTPException(status.HTTP_401_UNAUTHORIZED, detail=str(error)) 
+        case _:
+            return HTTPException(
+                status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro interno"
+            )
